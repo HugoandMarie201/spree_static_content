20c5c0dc6e59dee0a8447381bd57e88cd58de0fe
jfields <fieldsjustin@gmail.com>
2017-06-20 15:43:46 -0500

    Change admin static pages resource route from pages to static_pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.add_routes do
   namespace :admin, path: Spree.admin_path do
-    resources :pages
+    resources :pages, path: 'static_pages'
   end
   constraints(Spree::StaticPage) do
     get '/(*path)', to: 'static_content#show', as: 'static'
